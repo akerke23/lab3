077117dbcbcc3cd7f8b140875b54035089630c0f
akerke23 <myrzalyakerke02@gmail.com>
2022-09-27 21:24:06 +0600

    fourth

--- lab3.py
+++ lab3.py
@@ -1,4 +1,7 @@
-a = int(input())
-b = int(input())
-for i in range(a - (a + 1) % 2, b - b % 2, -2):
-    print(i, end=' ')
+n = int(input())
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+for i in range(n - 1):
+    sum -= int(input())
+print(sum)
