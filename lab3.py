946435e4822e746f22fd45db168548b1f9318d14
akerke23 <myrzalyakerke02@gmail.com>
2022-09-27 21:19:06 +0600

    third

--- lab3.py
+++ lab3.py
@@ -1,8 +1,4 @@
 a = int(input())
 b = int(input())
-if a < b:
-    for i in range(a, b + 1):
-        print(i)
-else:
-    for i in range(a, b - 1, -1):
-        print(i)
+for i in range(a - (a + 1) % 2, b - b % 2, -2):
+    print(i, end=' ')
