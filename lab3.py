4fa37835004d3cb65798db5aa86200edf61459af
akerke23 <myrzalyakerke02@gmail.com>
2022-09-28 14:05:26 +0600

    five

--- lab3.py
+++ lab3.py
@@ -1,7 +1,4 @@
-n = int(input())
-sum = 0
-for i in range(1, n + 1):
-    sum += i
-for i in range(n - 1):
-    sum -= int(input())
-print(sum)
+import random
+num1 = random.randint(1000, 9999)
+num2 = random.randrange(1000, 10000, 2)
+print(num1, num2)
